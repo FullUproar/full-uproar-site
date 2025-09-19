baa9f26def41b25e42e3f6ab8f0d1dd14d1f2319
Full Uproar <info@fulluproar.com>
2025-09-18 23:33:41 -0400

    Fix Fabric.js import for v6 compatibility

--- app/admin/design-components/components/CardDesigner.tsx
+++ app/admin/design-components/components/CardDesigner.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { fabric } from 'fabric';
+import * as fabric from 'fabric';
 import { adminStyles } from '../../styles/adminStyles';
 
 interface CardDimensions {
